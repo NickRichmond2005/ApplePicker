6deadde5b28cb7d1c3779e9a6abb46a63493f60a
NiRichm <thedeadkindo@gmail.com>
2025-09-18 16:59:08 -0500

    o;kmkFGHJ

--- Assets/LevelCounter.cs
+++ Assets/LevelCounter.cs
@@ -7,40 +7,19 @@ public class LevelCounter : MonoBehaviour
 {
     [Header("Dynamic")]
     public int score = 0;
-    
-    public AppleTree appleTree;
+
     
     private TextMeshProUGUI uiText;
 
     void Start()
     {
         uiText = GetComponent<TextMeshProUGUI>();
-
-        // GameObject appleTreeGO = GameObject.Find("AppleTree");
-        // appleTree = appleTreeGO.GetComponent<AppleTree>();
     }
 
     // Update is called once per frame
     void Update()
     {
         uiText.text = score.ToString("#,0");
-
-        // if (levelCounter.score == 1)
-        // {
-        //     appleTree.speed = 15f;
-        // }
-        // else if (levelCounter.score == 2)
-        // {
-        //     appleTree.speed = 20f;
-        // }
-        // else if (levelCounter.score == 3)
-        // {
-        //     appleTree.speed = 25f;
-        // }
-        // else if (levelCounter.score == 4)
-        // {
-        //     appleTree.speed = 35f;
-        // }
     }
 }
 
